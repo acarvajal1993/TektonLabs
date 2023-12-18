7b7f7c39f3df05cb609c7dd7b5e522aca5d27478
Argenis Carvajal <argenis24.udo@gmail.com>
2023-12-17 22:50:33 -0500

    Adding database to the project

---
+++ NET 7/TektonLabs.Infrastracture/Data/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TektonLabs.Infrastracture.Data;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public bool Status { get; set; }
+
+    public int Stock { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+}
